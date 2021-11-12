5cf1d6b21cc477b94260de886280be8014fc5df0
Kyle Henderson <ona@lunch.run>
2021-11-11 19:25:30 -0600

    format

--- ocaml_lox/bin/main.ml
+++ ocaml_lox/bin/main.ml
@@ -1,15 +1,13 @@
 open Core
+
 let file_param =
   let open Command.Param in
   anon (maybe ("filename" %: string))
 
 let runCode code =
-  code
-  |> Lox.Scanner.run
-  |> List.map ~f:(fun (t: Lox.Token.tokenInfo) -> t.token)
-  |> Lox.Parser.run
-  |> Lox.Expr.evaluate
-  |> Lox.Expr.show_literal
+  code |> Lox.Scanner.run
+  |> List.map ~f:(fun (t : Lox.Token.tokenInfo) -> t.token)
+  |> Lox.Parser.run |> Lox.Expr.evaluate |> Lox.Expr.show_literal
   |> Stdio.printf "Result: %s"
 
 let rec run_repl () =
@@ -19,24 +17,20 @@ let rec run_repl () =
   | None -> run_repl ()
   | Some "exit" -> ()
   | Some input ->
-    if phys_equal (String.strip input) "" then
-      run_repl ()
-    else 
-      runCode input;
+      if phys_equal (String.strip input) "" then run_repl () else runCode input;
       run_repl ()
 
 let run filename =
   match filename with
   | None -> run_repl ()
-  | Some filename -> 
-    let lines = In_channel.read_lines filename in
-    let code = List.fold ~init:"" ~f:(fun acc s -> acc ^ "\n" ^ s) lines in
-    let (): unit = printf "CODE: %s\n" code in
-    runCode code
+  | Some filename ->
+      let lines = In_channel.read_lines filename in
+      let code = List.fold ~init:"" ~f:(fun acc s -> acc ^ "\n" ^ s) lines in
+      let (() : unit) = printf "CODE: %s\n" code in
+      runCode code
 
 let command =
-  Command.basic
-    ~summary:"Lox language"
-    (Command.Param.map file_param ~f:(fun filename -> (fun () -> run filename)))
+  Command.basic ~summary:"Lox language"
+    (Command.Param.map file_param ~f:(fun filename () -> run filename))
 
 let () = Command.run command
